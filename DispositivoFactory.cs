2bc1805decdcc84b26ce7f9381498d479740a05a
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-09 00:17:04 +0000

    En la realidad se suelen crear clases que instancien el objeto por nosotros, para eso vamos a crear una factoria para abstraer al resto de la app de la creacion .

---
+++ DispositivoFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BasicConcepts
+{
+    public class DispositivoFactory
+    {
+        public enum TipoDispositivo { PC, Consola }
+
+        public static IDispoistivo CreateDispositivo(string tipo)
+        {
+            IDispoistivo dispositivo;
+
+            switch (tipo)
+            {
+                case "PC":
+                    dispositivo = new PC("Razer 9 5800, GTX 3070");
+                    break;
+                default:
+                    dispositivo = new Consola("PS5");
+                    break;
+            }
+
+            return dispositivo;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace BasicConcepts
     {
         public static void Main(string[] args)
         {
-            Jugador jugador = new Jugador("Edu", new PC("Razer 9 5800, GTX 3070"));
+            Jugador jugador = new Jugador("Edu", DispositivoFactory.CreateDispositivo("PC"));
             jugador.jugar();
 
-            Jugador jugador2 = new Jugador("Edu consolero", new Consola("PS5"));
+            Jugador jugador2 = new Jugador("Edu consolero", DispositivoFactory.CreateDispositivo("PS5"));
             jugador2.jugar();
         }
     }
