a9fe3216b780c2be88290adf8d9892e5e8980830
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-09 00:05:32 +0000

    Ahora podemos agregar desde el program otro jugador que juege a otro dispositivo de manera agil.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace BasicConcepts
         {
             Jugador jugador = new Jugador("Edu", new PC("Razer 9 5800, GTX 3070"));
             jugador.jugar();
+
+            Jugador jugador = new Jugador("Edu consolero", new Consola("PS%"));
+            jugador.jugar();
         }
     }
 }
